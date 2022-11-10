bbcc5c7d0376efc3840bde24d26613cb4bdf696f
Iman <iman@gits.id>
2022-11-10 11:32:47 +0700

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    host: env('DATABASE_HOST'),
+    port: env.int('DATABASE_PORT'),
+    database: env('DATABASE_NAME'),
+    user: env('DATABASE_USERNAME'),
+    password: env('DATABASE_PASSWORD'),
+  },
+});
