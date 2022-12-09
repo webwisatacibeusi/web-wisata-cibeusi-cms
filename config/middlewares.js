ece2128789cd235bc43e1bf7f66e02e9762b620e
Iman <imannurohman1@gmail.com>
2022-12-09 11:35:35 +0700

    Update for deploy to Northflank

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
+  'strapi::errors', {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
